206e5ec5b1565bc86e90215f68a954bdcc770a86
2-Elena-0 <e.nowitskaya2017@yandex.ru>
2025-06-08 22:21:16 +0300

    механика создания башни

--- Assets/Scripts/MapScript.cs
+++ Assets/Scripts/MapScript.cs
@@ -7,12 +7,14 @@ public class MapScript : MonoBehaviour
 {
 
     public bool IsTowerBuilding = false;
+    public GameObject newTower;
     
     private void Update()
     {
         if (Input.GetMouseButtonDown(0) && IsTowerBuilding)
         {
-            Debug.Log("Update");
+            Vector2 diference = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Instantiate(newTower, diference, Quaternion.identity);
             IsTowerBuilding = false;
         }
         
